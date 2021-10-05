96eb530f46aa98552e8becb54b2a457888b77b9c
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-10-05 23:34:24 +0700

    Setup react-webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function App() {
+	return (
+		<div>
+			<h1>Hello</h1>
+			<h2>Thao Nguyen</h2>
+		</div>
+	);
+}
+
+ReactDom.render(<App />, document.getElementById("root"));
